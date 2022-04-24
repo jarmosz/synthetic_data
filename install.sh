0920f529a1159386e8afb4c9bb9bf77bdf6ae780
Wojciech Jarmosz <jarmoszw@gmail.com>
2022-04-24 21:53:16 +0200

    Remove screen enable

--- install.sh
+++ install.sh
@@ -12,4 +12,3 @@ mkdir data && cd data
 wget https://minio.clarin-pl.eu/ermlab/public/PoLitBert/corpus-oscar/corpus_oscar_2020-04-10_64M_lines.zip
 unzip -p corpus_oscar_2020-04-10_64M_lines.zip | sed -r '/^\s*$/d' | gawk 'NF>6'  > oscar_filtered.txt
 split -l 1000000 --numeric-suffixes=1 --suffix-length=1 --additional-suffix=".txt"  oscar_filtered.txt ""
-screen -S generate_synt_data
